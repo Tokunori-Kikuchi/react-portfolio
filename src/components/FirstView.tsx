4a6bd3f1b81a245214fba8717d39b64e5b717629
Tokunori-Kikuchi <eleven9terror@gmail.com>
2025-06-22 19:21:14 +0900

    FirstView.tsx変更

--- src/components/FirstView.tsx
+++ src/components/FirstView.tsx
@@ -1,21 +1,71 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { Link as ScrollLink } from 'react-scroll';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import { worksData } from './worksData';
 
 const FirstView = () => {
   return (
     <div id="fv" className="fv">
-      <picture className="fv-img">
-        <source media="(min-width: 768px)" srcSet="/images/img_fv.png" />
-        <img src="/images/sp/img_fv.png" alt="スマートフォン版ファーストビュー" loading="lazy"/>
-      </picture>
-      <div className="fv-contents">
-        <h1 className="fv-heading">
-          <span className="fv-heading-main">Web Corder -</span>
-          <span className="fv-heading-sub">ご覧いただきありがとうございます。<br />WEBコーダーをしているTOKUと申します。</span>
-        </h1>
+      <div className="fv-container">
+        <div className="fv-left">
+          <p className="fv-left-sub-text">Webコーダー</p>
+          <h1 className="fv-left-main-text">TOKU's PORTFOLIO</h1>
+          <p className="fv-left-description">
+            静的なHTMLコーディングからWordPress化まで対応可能。
+            <br />
+            デザインカンプの意図をしっかりと理解した上で、迅速、丁寧な対応コーディング業務をサポート！
+          </p>
+        </div>
+        <div className="fv-right">
+            <a href="https://twitter.com/your_twitter_account" target="_blank" rel="noopener noreferrer" className="follow-us">
+                FOLLOW US
+            </a>
+        </div>
+      </div>
+
+      <Swiper
+        modules={[Pagination, Autoplay]}
+        slidesPerView={'auto'}
+        centeredSlides={true}
+        spaceBetween={30}
+        pagination={{ clickable: true }}
+        loop={true}
+        autoplay={{ delay: 3000, disableOnInteraction: false }}
+        className="fv-swiper"
+      >
+        {worksData.map((work) => (
+          <SwiperSlide key={work.id}>
+            <div className="slide-inner-content">
+              <div className="slide-text-content">
+                <img src={work.logo} alt={`${work.title} logo`} className="work-logo" />
+                <h3 className="work-title">{work.title}</h3>
+                <div className="work-tags">
+                  {work.tags.map((tag, index) => (
+                    <span key={index} className="work-tag">{tag}</span>
+                  ))}
+                </div>
+              </div>
+              <div className="slide-visual-content">
+                <img src={work.image} alt={work.title} className="work-image" />
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <div className="fv-contact-button-container">
+        <ScrollLink to="contact" smooth={true} duration={500} offset={-80} className="fv-contact-button">
+            Contact
+            <span className="arrow-circle">
+                <svg width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.5 1L6.5 6L1.5 11" stroke="#065471" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/></svg>
+            </span>
+        </ScrollLink>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FirstView
+export default FirstView;
---
+++ src/components/worksData.ts
@@ -0,0 +1,99 @@
+export interface Work {
+  id: number;
+  logo: string;
+  title: string;
+  image: string;
+  url: string;
+  tags: string[];
+}
+
+export const worksData: Work[] = [
+    {
+      id: 1,
+      logo: '/images/logo.png',
+      title: 'ｔｏｃｏｔｏｎ様公式サイト',
+      image: '/images/img_works001.jpg',
+      url: 'https://tocoton-tokyo.com/',
+      tags: ['HTML', 'CSS', 'JavaScript', 'WordPress'],
+    },
+    {
+      id: 2,
+      logo: '/images/logo.png',
+      title: '仮想スイーツ店公式サイト（サンプル）',
+      image: '/images/img_works002.jpg',
+      url: 'https://webwisewords.net//works/sweet_delight/index.html',
+      tags: ['HTML', 'CSS', 'JavaScript'],
+    },
+    {
+      id: 3,
+      logo: '/images/logo.png',
+      title: '仮想企業のLP',
+      image: '/images/img_works003.jpg',
+      url: 'https://webwisewords.net/works/The_Company/index.html',
+      tags: ['HTML', 'CSS', 'JavaScript'],
+    },
+    {
+      id: 4,
+      logo: '/images/logo.png',
+      title: 'sobolon様(デイトラ制作物)',
+      image: '/images/img_works004.jpg',
+      url: 'https://webwisewords.net/works/sobolon/index.html',
+      tags: ['HTML', 'CSS', 'JavaScript'],
+    },
+    {
+      id: 5,
+      logo: '/images/logo.png',
+      title: '仮想企業のLP',
+      image: '/images/img_works005.jpg',
+      url: 'https://webwisewords.net/works/Sample_Corp/index.html',
+      tags: ['HTML', 'CSS', 'JavaScript'],
+    },
+    {
+      id: 6,
+      logo: '/images/logo.png',
+      title: '仮想企業のWebサイト（WordPress）',
+      image: '/images/img_works006.jpg',
+      url: 'https://totonou-code.com/pacificmall/',
+      tags: ['HTML', 'CSS', 'JavaScript', 'WordPress'],
+    },
+    {
+      id: 7,
+      logo: '/images/logo.png',
+      title: 'オリジナルテーマブログ（WordPress）',
+      image: '/images/img_works007.jpg',
+      url: 'https://totonou-code.com/TF-30/',
+      tags: ['HTML', 'CSS', 'JavaScript', 'PHP', 'WordPress'],
+    },
+    {
+      id: 8,
+      logo: '/images/logo.png',
+      title: '仮想企業 + アプリe-tigaのLP',
+      image: '/images/img_works008.jpg',
+      url: 'https://webwisewords.net/works/e-tiga/index.html',
+      tags: ['HTML', 'SCSS', 'JavaScript'],
+    },
+    {
+      id: 9,
+      logo: '/images/logo.png',
+      title: '仮想アプリOHAのLP',
+      image: '/images/img_works009.jpg',
+      url: 'https://webwisewords.net/works/oha/index.html',
+      tags: ['HTML', 'CSS', 'JavaScript'],
+    },
+    {
+      id: 10,
+      logo: '/images/logo.png',
+      title: '喫茶店WEB_CAFEのHP',
+      image: '/images/img_works010.jpg',
+      url: 'https://webwisewords.net/works/WEB_CAFE/index.html',
+      tags: ['HTML', 'CSS', 'JavaScript'],
+    },
+    {
+      id: 11,
+      logo: '/images/logo.png',
+      title: '初めて制作したLP',
+      image: '/images/img_works999.jpg',
+      url: 'https://webwisewords.net/works/first_LP/index.html',
+      tags: ['HTML', 'CSS', 'JavaScript'],
+    },
+  ];
